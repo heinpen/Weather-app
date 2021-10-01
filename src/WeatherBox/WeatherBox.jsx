aa2451874950d25ab00463b06de47192e685dbc7
Oleksandr Lishchynskyi <sashalischinskiy72@gmail.com>
2021-10-01 19:28:19 +0300

    Add WeatherBox component

---
+++ src/WeatherBox/WeatherBox.jsx
@@ -0,0 +1,31 @@
+import Search from "../Search/Search";
+import Info from "../Info/Info";
+
+// import "./weather-box.css";
+import { useState } from "react";
+
+const api = {
+  key: "af342bb316a547e15be0a79e5bb55c58",
+  base: "https://api.openweathermap.org/data/2.5/",
+};
+function WeatherBox() {
+  const [weather, setWeather] = useState({});
+  const [query, setQuery] = useState("");
+  console.log(query);
+  function search() {
+    fetch(`${api.base}weather?q=${query}&units=metric&APPID=${api.key}`)
+      .then((res) => res.json())
+      .then((result) => {
+        setWeather(result);
+      });
+  }
+
+  return (
+    <div className="weather-box">
+      <Search search={search} setQuery={setQuery} />
+      <Info weather={weather} />
+    </div>
+  );
+}
+
+export default WeatherBox;
