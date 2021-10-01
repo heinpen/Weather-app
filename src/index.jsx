73f6eb986b83b6e6346ec90133f4e02a3278f806
Oleksandr Lishchynskyi <sashalischinskiy72@gmail.com>
2021-10-01 19:25:32 +0300

    Add main jsx file

---
+++ src/index.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import WeatherBox from "./WeatherBox/WeatherBox";
+
+ReactDOM.render(
+  <React.StrictMode>
+    <WeatherBox />
+  </React.StrictMode>,
+  document.getElementById("root")
+);
