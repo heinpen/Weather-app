224c2fb2221c8fe83fbba871f64ca8d797ff388e
Oleksandr Lishchynskyi <sashalischinskiy72@gmail.com>
2021-10-04 15:22:36 +0300

    Refactor code according to eslint rules

--- src/WeatherBox/WeatherBox.jsx
+++ src/WeatherBox/WeatherBox.jsx
@@ -1,16 +1,16 @@
-import Search from "../Search/Search";
-import Info from "../Info/Info";
+import { useState } from 'react';
+import Search from '../Search/Search';
+import Info from '../Info/Info';
 
 // import "./weather-box.css";
-import { useState } from "react";
 
 const api = {
-  key: "af342bb316a547e15be0a79e5bb55c58",
-  base: "https://api.openweathermap.org/data/2.5/",
+  key: 'af342bb316a547e15be0a79e5bb55c58',
+  base: 'https://api.openweathermap.org/data/2.5/',
 };
 function WeatherBox() {
   const [weather, setWeather] = useState({});
-  const [query, setQuery] = useState("");
+  const [query, setQuery] = useState('');
   console.log(query);
   function search() {
     fetch(`${api.base}weather?q=${query}&units=metric&APPID=${api.key}`)
@@ -22,7 +22,7 @@ function WeatherBox() {
 
   return (
     <div className="weather-box">
-      <Search search={search} setQuery={setQuery} />
+      <Search search={() => search} setQuery={setQuery} />
       <Info weather={weather} />
     </div>
   );
