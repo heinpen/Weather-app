4f4cf26629b0ab0da853639372749e6857944c47
Oleksandr Lishchynskyi <sashalischinskiy72@gmail.com>
2021-10-05 11:13:56 +0300

    Add geolocation functionality

--- src/WeatherBox/WeatherBox.jsx
+++ src/WeatherBox/WeatherBox.jsx
@@ -1,18 +1,49 @@
-import { useState } from 'react';
+import { useState, useEffect, useRef } from 'react';
 import Search from '../Search/Search';
 import Info from '../Info/Info';
 
-// import "./weather-box.css";
-
 const api = {
-  key: 'af342bb316a547e15be0a79e5bb55c58',
+  key: '1fd834a005e103644a2bc3e131f251ab',
   base: 'https://api.openweathermap.org/data/2.5/',
 };
+
 function WeatherBox() {
   const [weather, setWeather] = useState({});
   const [query, setQuery] = useState('');
-  console.log(query);
-  function search() {
+
+  const mounted = useRef(false);
+  if (!mounted.current) {
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(getUserPosition, handleError);
+    } else {
+      handleError();
+    }
+  }
+
+  function handleError(error) {
+    error
+      ? setWeather({ message: 'Your browser is not supporting geolocation' })
+      : setWeather({ message: error.message });
+  }
+
+  useEffect(() => {
+    mounted.current = true;
+  }, []);
+
+  function getUserPosition(position) {
+    const { longitude, latitude } = position.coords;
+    searchForUserWeather(longitude, latitude);
+  }
+
+  function searchForUserWeather(longitude, latitude) {
+    fetch(`${api.base}weather?lat=${latitude}&lon=${longitude}&&units=metric&APPID=${api.key}`)
+      .then((res) => res.json())
+      .then((result) => {
+        setWeather(result);
+      });
+  }
+
+  function searchForWeather() {
     fetch(`${api.base}weather?q=${query}&units=metric&APPID=${api.key}`)
       .then((res) => res.json())
       .then((result) => {
@@ -22,7 +53,7 @@ function WeatherBox() {
 
   return (
     <div className="weather-box">
-      <Search search={() => search()} setQuery={setQuery} />
+      <Search search={() => searchForWeather()} setQuery={setQuery} />
       <Info weather={weather} />
     </div>
   );
