87275f71345f426031f70391a339929164d68d43
Oleksandr Lishchynskyi <sashalischinskiy72@gmail.com>
2021-10-05 11:52:19 +0300

    Format code according to eslint

--- src/index.jsx
+++ src/index.jsx
@@ -1,11 +1,12 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import "./index.css";
-import WeatherBox from "./WeatherBox/WeatherBox";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import WeatherBox from './WeatherBox/WeatherBox';
 
 ReactDOM.render(
   <React.StrictMode>
     <WeatherBox />
   </React.StrictMode>,
-  document.getElementById("root")
+
+  document.getElementById('root'),
 );
